3b5651e003903053769b0413ea2f950243432924
Saverin <eugeneforce@proton.me>
2026-06-15 18:53:00 +0100

    waste of time

--- src/NetworkManager.zig
+++ src/NetworkManager.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
 pub const NMClient = opaque {};
 const utils = @import("utils.zig");
-const GError = struct {};
+const GError = struct { quark: c_uint, code: c_int, message: [*:0]const u8 };
 const GCancellable = opaque {};
 const NMWifiP2PPeer = opaque {};
+const NMSetting = opaque {};
 const NMActiveConnection = opaque {};
 pub const NMDevice = opaque {};
 const GBytes = opaque {};
@@ -40,6 +41,18 @@ extern fn nm_wifi_p2p_peer_get_wfd_ies(_: ?*NMWifiP2PPeer) callconv(.c) ?*GBytes
 extern fn nm_wifi_p2p_peer_get_name(_: ?*NMWifiP2PPeer) callconv(.c) [*:0]const u8;
 extern fn g_timeout_add_seconds(_: c_uint, _: ?*const fn (?*anyopaque) callconv(.c) c_int, _: ?*anyopaque) callconv(.c) c_uint;
 extern fn nm_wifi_p2p_peer_get_hw_address(_: ?*NMWifiP2PPeer) callconv(.c) [*:0]const u8;
+extern fn nm_setting_connection_new() callconv(.c) ?*NMSetting;
+extern fn nm_setting_connection_add_permission(_: ?*anyopaque, _: ?[*:0]const u8, _: ?[*:0]const u8, _: ?[*:0]const u8) callconv(.c) c_int;
+extern fn nm_setting_wifi_p2p_new() callconv(.c) ?*NMSetting;
+extern fn nm_setting_ip4_config_new() callconv(.c) ?*NMSetting;
+extern fn nm_setting_ip6_config_new() callconv(.c) ?*NMSetting;
+extern fn nm_simple_connection_new() callconv(.c) ?*anyopaque;
+extern fn nm_connection_add_setting(_: ?*anyopaque, _: ?*NMSetting) callconv(.c) void;
+extern fn g_bytes_new_static(_: ?[*:0]const u8, _: c_ulong) callconv(.c) ?*GBytes;
+extern fn nm_client_add_and_activate_connection2(_: ?*NMClient, _: ?*anyopaque, _: ?*NMDevice, _: ?[*:0]const u8, _: ?*anyopaque, _: ?*GCancellable, _: GAsyncReadyCallback, _: ?*anyopaque) callconv(.c) void;
+extern fn g_object_set(object: ?*anyopaque, first_property_name: [*:0]const u8, ...) callconv(.c) void;
+extern fn nm_object_get_path(_: ?*anyopaque) callconv(.c) [*:0]const u8;
+extern fn nm_client_add_and_activate_connection2_finish(_: ?*NMClient, _: ?*anyopaque, _: ?**anyopaque, _: *?*GError) callconv(.c) ?*anyopaque;
 
 pub fn configure(app: *App) void {
     var _gerror: *GError = undefined;
@@ -47,6 +60,7 @@ pub fn configure(app: *App) void {
     if (client == null) {
         std.log.err("{s}", .{"Could not create NetworkManager client"});
     }
+    app.client = client;
     const devices = nm_client_get_devices(client);
     for (0..devices.len) |i| {
         const device: *NMDevice = @ptrCast(devices.pdata[i]);
@@ -74,7 +88,7 @@ fn getPeers(device: *NMDevice, app: *App) void {
         device,
         "peer-added",
         peer_added_cb,
-        null,
+        app,
         null,
         0,
     );
@@ -83,11 +97,11 @@ fn getPeers(device: *NMDevice, app: *App) void {
         device,
         "peer-removed",
         peer_removed_cb,
-        null,
+        app,
         null,
         0,
     );
-    if (!app.discover) {
+    if (app.stream) {
         std.log.info("Searching for a peer with hardware address {s}", .{app.target_hw_address.?});
     }
     nm_device_wifi_p2p_start_find(device, null, null, log_start, null); //dies after 30 seconds
@@ -95,7 +109,7 @@ fn getPeers(device: *NMDevice, app: *App) void {
     const existingPeers = nm_device_wifi_p2p_get_peers(device);
     for (0..existingPeers.len) |p| {
         const peer: *NMWifiP2PPeer = @ptrCast(existingPeers.pdata[p]);
-        peer_added_cb(device, peer, null);
+        peer_added_cb(device, peer, app);
     }
 }
 
@@ -111,20 +125,66 @@ fn peer_added_cb(
     peer: ?*NMWifiP2PPeer,
     user_data: ?*anyopaque,
 ) callconv(.c) void {
-    _ = user_data;
-    _ = device;
+    const app: *App = @ptrCast(@alignCast(user_data.?));
     std.log.debug("peer-added event fired", .{});
     const wfd_ies = nm_wifi_p2p_peer_get_wfd_ies(peer);
     if (wfd_ies == null) {
         return;
     }
     const peer_name = nm_wifi_p2p_peer_get_name(peer);
-    const hardware_address = nm_wifi_p2p_peer_get_hw_address(peer);
+    const hardware_address = std.mem.span(nm_wifi_p2p_peer_get_hw_address(peer));
     std.log.info("Discovered peer with name {s} and hardware address {s}", .{ peer_name, hardware_address });
 
-    if (hardware_address == hardware_address) {
-        std.log.info("Found target device. Beginning stream on {s}", .{peer_name});
+    if (app.stream and std.mem.order(u8, app.target_hw_address.?, hardware_address) == .eq) {
+        std.log.info("Found target device. Starting stream", .{});
+        startStream(device, app, peer);
     }
+}
+
+fn startStream(device: ?*NMDevice, app: *App, peer: ?*NMWifiP2PPeer) void {
+    std.log.debug("Connecting to device", .{});
+    const client = app.client;
+    const connection = nm_simple_connection_new();
+    // const general_setting = nm_setting_connection_new();
+    // nm_setting_connection_add_permission(general_setting, "user", "noble", null);
+    // nm_connection_add_setting(connection, general_setting);
+    // g_object_set(general_setting,"zone",)
+
+    const wfd_ies = g_bytes_new_static("\x00\x00\x06\x00\x90\x1c\x44\x00\xc8", 9);
+    const p2p_setting = nm_setting_wifi_p2p_new();
+    nm_connection_add_setting(connection, p2p_setting);
+    g_object_set(p2p_setting, "wfd-ies", wfd_ies, @as(?*anyopaque, null));
+
+    const ipv4_setting = nm_setting_ip4_config_new();
+    nm_connection_add_setting(connection, ipv4_setting);
+    g_object_set(ipv4_setting, "method", "auto", "never-default", @as(c_int, 1), @as(?*anyopaque, null));
+
+    const ipv6_setting = nm_setting_ip6_config_new();
+    nm_connection_add_setting(connection, ipv6_setting);
+    g_object_set(ipv6_setting, "method", "auto", "never-default", @as(c_int, 1), "may-fail", @as(c_int, 1), @as(?*anyopaque, null));
+
+    nm_client_add_and_activate_connection2(client, connection, device, nm_object_get_path(peer), null, null, p2p_connected, app);
+}
+
+fn p2p_connected(
+    source: ?*anyopaque,
+    result: ?*anyopaque,
+    user_data: ?*anyopaque,
+) callconv(.c) void {
+    _ = user_data;
+    var err: ?*GError = null;
+    const active = nm_client_add_and_activate_connection2_finish(
+        @ptrCast(source),
+        result,
+        null,
+        &err,
+    );
+    _ = active;
+    if (err) |e| {
+        std.log.err("P2P connection failed: {s}", .{e.message});
+        return;
+    }
+    std.log.info("P2P activation started", .{});
 }
 
 fn peer_removed_cb(
--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,8 @@ const NetworkManager = @import("NetworkManager.zig");
 const utils = @import("utils.zig");
 const App = @This();
 const GMainLoop = opaque {};
-discover: bool = false, //false if we're looking to stream instead of just browsing
+stream: bool = false,
+client: ?*NetworkManager.NMClient = null,
 target_hw_address: ?[]const u8 = null,
 extern fn g_main_loop_new(_: ?*anyopaque, _: c_int) callconv(.c) *GMainLoop;
 extern fn g_main_loop_run(_: ?*GMainLoop) callconv(.c) void;
@@ -13,12 +14,16 @@ pub fn main(init: std.process.Init.Minimal) !void {
 
     var app = App{};
     if (args.len < 2) utils.die("{s}", .{"Not enough arguments supplied"});
-    if (std.mem.order(u8, args[1], "--discover") == .eq) {
-        app.discover = true;
-    } else if (std.mem.order(u8, args[1], "--stream") == .eq and args.len > 2) {
-        app.discover = false;
+    if (std.mem.order(u8, args[1], "--discover") == .eq) {} else if (std.mem.order(u8, args[1], "--stream") == .eq) {
+        if (args.len < 3) utils.die("{s}", .{"Must provide target hardware address"});
+        app.stream = true;
         app.target_hw_address = args[2];
-    }
+        const target_len = std.mem.trim(u8, app.target_hw_address.?, " ").len;
+        std.log.info("{d}", .{target_len});
+        if (target_len == 0) {
+            utils.die("{s}", .{"Must provide target hardware address"});
+        }
+    } else {}
     const loop = g_main_loop_new(null, 0);
     configureNetwork(&app);
 
